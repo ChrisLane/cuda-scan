8a6ae6a55dfbd59dd9829c8aa274308a007a5ff6
Chris Lane <chris@chrislane.com>
2018-02-22 13:10:49 +0000

    Add block scan working up to array size 512

--- src/main.cu
+++ src/main.cu
@@ -52,53 +52,138 @@ struct __cuda_fake_struct {
 
 // For the CUDA runtime routines (prefixed with "cuda_")
 #include <cuda_runtime.h>
+#include <helper_cuda.h>
 
-__global__ void scanExclusive(int *g_out, int *g_in, int len) {
-    extern __shared__ int temp[]; // allocated on invocation
-    int threadId = threadIdx.x;
-    int pout = 0, pin = 1;
-    // Load input into shared memory.
-    // This is exclusive scan, so shift right by one
-    // and set first element to 0
-    temp[pout * len + threadId] = (threadId > 0) ? g_in[threadId - 1] : 0;
-    __syncthreads();
-    for (int offset = 1; offset < len; offset *= 2) {
-        pout = 1 - pout; // swap double buffer indices
-        pin = 1 - pout;
-        if (threadId >= offset) {
-            temp[pout * len + threadId] += temp[pin * len + threadId - offset];
-        } else {
-            temp[pout * len + threadId] = temp[pin * len + threadId];
-        }
-        __syncthreads();
+#define NUM_BANKS 32
+
+void refScan(int *h_output, int *h_input, const int len) {
+    h_output[0] = 0;
+
+    for (int i = 1; i < len; ++i) {
+        h_output[i] = h_input[i - 1] + h_output[i - 1];
     }
-    g_out[threadId] = temp[pout * len + threadId]; // write output
+}
+
+extern __shared__ int temp[]; // allocated on invocation
+__global__ void blockScan(int *d_Output, int *d_Input, const int len) {
+    int thid = threadIdx.x;
+    int offset = 1;
+
+    if (2 * thid < len) {
+        temp[2 * thid] = d_Input[2 * thid]; // load input into shared memory
+    }
+    if (2 * thid + 1 < len) {
+        temp[2 * thid + 1] = d_Input[2 * thid + 1];
+    }
+
+    for (int d = len >> 1; d > 0; d >>= 1) { // build sum in place up the tree
+        __syncthreads();
+        if (thid < d) {
+            int ai = offset * (2 * thid + 1) - 1;
+            int bi = offset * (2 * thid + 2) - 1;
+
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
+
+    if (thid == 0) {
+        temp[len - 1] = 0; // clear the last element
+    }
+
+    for (int d = 1; d < len; d *= 2) { // traverse down tree & build scan
+        offset >>= 1;
+        __syncthreads();
+        if (thid < d) {
+            int ai = offset * (2 * thid + 1) - 1;
+            int bi = offset * (2 * thid + 2) - 1;
+
+            int t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+    __syncthreads();
+
+    if (2 * thid < len) {
+        d_Output[2 * thid] = temp[2 * thid]; // write results to device memory
+    }
+    if (2 * thid + 1 < len) {
+        d_Output[2 * thid + 1] = temp[2 * thid + 1];
+    }
+}
+
+void printTestEquals(int *value1, int *value2, int len) {
+    bool equal = true;
+    for (int i = 0; i < len; i++) {
+        if (value1[i] != value2[i]) {
+            equal = false;
+        }
+    }
+
+    printf("Test: %s\n", equal ? "PASS" : "FAIL" );
 }
 
 int main() {
+    int blockSize;
+    int minGridSize;
+    int gridSize;
+
     int *h_Input;
+    int *h_Output;
+    int *h_Output_d;
     int *d_Input;
     int *d_Output;
-    int length = 100; // Number of elements in the input array.
+    const int len = 512; // Number of elements in the input array.
 
-    // Allocate hardware memory
-    h_Input = (int *) malloc(length * sizeof(int));
+    // Allocate host memory
+    printf("Allocating host memory...\n");
+    h_Input = (int *) malloc(len * sizeof(int));
+    h_Output = (int *) malloc(len * sizeof(int));
+    h_Output_d = (int *) malloc(len * sizeof(int));
 
     // Generate random integers to create input
+    printf("Generating random input...\n");
     srand((uint) (time(NULL)));
-    for (int i = 0; i < length; i++) {
-        h_Input[i] = rand() % 10;
+    for (int i = 0; i < len; i++) {
+        h_Input[i] = 1; //rand() % 10;
     }
 
     // Allocate device memory and copy input to device
-    cudaMalloc((void **) &d_Input, length * sizeof(int));
-    cudaMalloc((void **) &d_Output, length * sizeof(int));
-    cudaMemcpy(d_Input, h_Input, length * sizeof(int), cudaMemcpyHostToDevice);
+    printf("Allocating device memory...\n");
+    checkCudaErrors(cudaMalloc((void **) &d_Input, len * sizeof(int)));
+    checkCudaErrors(cudaMalloc((void **) &d_Output, len * sizeof(int)));
+    printf("Copying input to device...\n");
+    checkCudaErrors(cudaMemcpy(d_Input, h_Input, len * sizeof(int), cudaMemcpyHostToDevice));
 
-    printf("%d", h_Input[2]);
+    // Set Grid and Block sizes
+    checkCudaErrors(cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, blockScan, 0, 0));
+    gridSize = (len + blockSize - 1) / blockSize;
+
+    // Run Single block scan
+    //printf("Grid: %d, Block %d\n", gridSize, blockSize);
+    blockScan << < 1, blockSize, 2 * blockSize >> > (d_Output, d_Input, len);
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    refScan(h_Output, h_Input, len);
+    /*printf("\nReference Result:\n");
+    for (int i = 0; i < len; i++) {
+        printf("%d ", h_Output[i]);
+    }
+    printf("\n");*/
+
+    checkCudaErrors(cudaMemcpy(h_Output_d, d_Output, len * sizeof(int), cudaMemcpyDeviceToHost));
+    /*printf("Block Scan Result:\n");
+    for (int i = 0; i < len; i++) {
+        printf("%d ", h_Output_d[i]);
+    }
+    printf("\n");*/
+    printTestEquals(h_Output, h_Output_d, len);
 
     // Clean up memory
+    printf("Cleaning up memory...\n");
     free(h_Input);
-    cudaFree(d_Output);
-    cudaFree(d_Input);
+    free(h_Output_d);
+    checkCudaErrors(cudaFree(d_Output));
+    checkCudaErrors(cudaFree(d_Input));
 }
